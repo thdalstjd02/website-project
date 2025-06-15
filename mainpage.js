457e42f378ac5dfdc28012d446af23efc780f92d
minseong song <vexx045@gmail.com>
2025-06-15 08:23:14 +0000

    sports marketing program

--- mainpage.js
+++ mainpage.js
@@ -1,14 +1,11 @@
-// mainpage.js (수정된 최종 코드)
-
 window.addEventListener('load', () => {
     const volleyballIcon = document.getElementById('volleyball-icon');
 
-    // 배구공 애니메이션('bounce')이 끝나면 실행됩니다.
-    volleyballIcon.addEventListener('animationend', () => {
-
-        // 애니메이션이 끝난 후, 'about.html' 페이지로 이동합니다.
-        // 여기가 핵심 수정 부분입니다!
-        window.location.href = 'about.html'; 
-
-    });
+    if (volleyballIcon) {
+        // 배구공 애니메이션('bounce')이 끝나면 실행됩니다.
+        volleyballIcon.addEventListener('animationend', () => {
+            // 애니메이션이 끝난 후, 'index.html'(홈페이지)로 이동합니다.
+            window.location.href = 'index.html'; 
+        });
+    }
 });
